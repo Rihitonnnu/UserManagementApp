896988c9fd2a37ffc2688be66f65f31e1dc7b8ae
rihito <hexiliang04@gmail.com>
2022-01-04 01:18:05 +0900

    ホームページと設定ページをリニューアル
    簡素なものだったためなにか案内を加えたかったから

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,23 @@
-import { VFC ,memo } from "react";
+import styled from "@emotion/styled";
+import { VFC, memo } from "react";
+import { InstructionMessage } from "../atoms/Message/InstructionMessage";
 
-export const Home:VFC=memo(()=>{
-    return <p>ホームページです</p>
+export const Home: VFC = memo(() => {
+    return (
+        <>
+            <InstructionMessage>                    
+                この度はユーザー管理アプリにログインしていただきありがとうございます。<br></br>当アプリでは
+                ユーザー一覧を閲覧したり、管理者権限を用いてデータの変更を行うことができます。<br></br>
+                このアプリのユーザー情報は<Sa href="https://jsonplaceholder.typicode.com/">JSONPlaceholder</Sa>
+                を利用して取得しております。
+            </InstructionMessage>
+        </>
+    )
 })
+
+const Sa=styled.a`
+text-decoration:underline;
+&:hover{
+    color:red
+}
+`
--- src/components/pages/Setting.tsx
+++ src/components/pages/Setting.tsx
@@ -1,5 +1,10 @@
-import { VFC ,memo } from "react";
+import { VFC, memo } from "react";
+import { InstructionMessage } from "../atoms/Message/InstructionMessage";
 
-export const Setting:VFC=memo(()=>{
-    return <p>設定ページです</p>
+export const Setting: VFC = memo(() => {
+    return (
+        <InstructionMessage>
+            こちらは設定ページです。設定ページは今後実装予定です。
+        </InstructionMessage>
+    )
 })
